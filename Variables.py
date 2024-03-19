774f670f6cf120bf756ac6cb0b3914fed6a0ff08
alejoboga20 <alejoboga19@gmail.com>
2024-03-19 15:47:53 -0300

    add variable files

---
+++ Variables.py
@@ -0,0 +1,13 @@
+"""
+Variables.py
+"""
+
+cost = 10 # Integer
+tax_percent = .25 # Float
+tax = cost * tax_percent
+price = cost + tax
+
+print(price)
+
+username = "John Doe" # String
+print(username)
